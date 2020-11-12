53edb1b23592ff7cea027856a5fca9b85e870c62
kelamoana <kela.moana@gmail.com>
2020-11-12 15:12:18 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,12 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+const tl = new TimelineMax();
+
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body');
+		$body = $('body'); 
 
 	// Breakpoints.
 		breakpoints({
